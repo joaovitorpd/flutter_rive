807f8b6d02db93c7b4d43f6b77e7ea5bd816eb5e
João <joaovitorpd@gmail.com>
2021-11-30 10:54:44 -0300

    first commit

---
+++ android/app/src/main/kotlin/jvpd/com/br/flutter_rive/MainActivity.kt
@@ -0,0 +1,6 @@
+package jvpd.com.br.flutter_rive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          child: const RiveAnimation.asset("assets/AnimGears.riv", animations: ["spin1"],),
+        ),
+      ),
+    );
+  }
+}
